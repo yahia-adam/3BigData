e65fdc4fbc898d5de92cf35fd3ec9d5ea16f18fe
YAHIA ABDCHAFEE Adam <adam.y.abdc@gmail.com>
2024-03-24 02:37:20 +0100

    add linear v1

--- mylib/examples/linear_model.rs
+++ mylib/examples/linear_model.rs
@@ -9,42 +9,45 @@
 /*   Updated: 2024/03/22 14:20:18 by YA. Adam             ########## ########   ######## ###########         */
 /*                                                                                                           */
 /* ********************************************************************************************************* */
-
-extern crate mylib;
+// extern crate mylib;
 use mylib::linear_model::LinearModel;
+
+fn celsius_to_fahrenheit(value: f32) -> f32 {
+    value * 1.8 + 32.0
+}
 
 fn main()
 {
+    // let name = env::args().skip(1).next();
+    // println!("Hello, {}!", name.unwrap_or("world".into()));
     let mut sp = LinearModel::new();
-    let celsius_temperatures = vec![
-        vec![0.0], vec![11.0], vec![27.0], vec![6.0],
-        vec![13.0], vec![39.0], vec![19.0], vec![25.0],
-        vec![30.0], vec![-15.0], vec![14.0], vec![33.0],
-        vec![-5.0], vec![20.0], vec![22.0], vec![-10.0],
-        vec![17.0], vec![37.0], vec![24.0], vec![-20.0],
-        vec![8.0], vec![35.0], vec![-30.0], vec![21.0],
-        vec![10.0], vec![-25.0], vec![-2.0], vec![-40.0],
-        vec![16.0], vec![29.0], vec![7.0], vec![-35.0],
-        vec![36.0], vec![-22.0], vec![-38.0], vec![28.0],
-        vec![-45.0], vec![38.0], vec![3.0], vec![18.0],
-        vec![-12.0], vec![-18.0], vec![32.0], vec![-8.0],
-        vec![15.0], vec![2.0], vec![-33.0], vec![26.0],
-        vec![-50.0], vec![12.0], vec![-3.0], vec![5.0],
-    ];
-    
-    let fahrenheit_temperatures = vec![
-        32.000, 51.800, 80.600, 42.800, 55.400, 102.200, 66.20, 77.0, 87.8,
-        5.0, 57.2, 91.4, 23.0, 68.0, 71.6, 14.0, 62.6, 98.6, 75.2, -4.0,
-        46.4, 95.0, -22.0, 69.8, 50.0, -13.0, 28.4, -40.0, 60.8, 84.2, 
-        44.6, 84.2, 102.2, 37.4, 64.4, 105.8, 39.2, 14.0, 17.6, 89.6,
-        46.4, 30.2, -22.0, 27.0, 21.2, -27.4, -25.6, 73.4, -9.4, -40.0,
-        -8.0, 91.4, -26.0, -37.4, -16.6, -54.2,
-    ];
-    
-    let datasets = (&celsius_temperatures, &fahrenheit_temperatures);
-    sp.fit(datasets, 0.0001, 5, 0.5);
-    // println!("5C = {}F", sp.guess(&[1.0, 5.0]));
-    println!("{}", sp.to_string());
 
-    // let res = sp.save("../models/firstmodel.txt");
+    let celsius_temperatures: Vec<f32> = vec![ 0.0, 11.0, 27.0, 6.0, 13.0, 39.0, 19.0, 25.0, 30.0, -15.0, 14.0, 33.0, -5.0, 20.0, 22.0, -10.0, 17.0, 37.0, 24.0, -20.0, 8.0, 35.0, -30.0, 21.0, 10.0, -25.0, -2.0, -40.0, 16.0, 29.0, 7.0, -35.0, 36.0, -22.0, -38.0, 28.0, -45.0, 38.0, 3.0, 18.0, -12.0, -18.0, 32.0, -8.0, 15.0, 2.0, -33.0, 26.0, -50.0, 12.0, -3.0, 5.0];
+    let celsius_temperatures_row: usize = celsius_temperatures.len();
+    let celsius_temperatures_col:usize = 1;
+
+    let mut fahrenheit_temperatures: Vec<f32> = vec![];
+    for i in &celsius_temperatures {
+        fahrenheit_temperatures.push(celsius_to_fahrenheit(*i));
+    }
+
+    let fahrenheit_temperatures_row: usize = fahrenheit_temperatures.len();
+    let leaked_celsius_temperatures = Vec::leak(celsius_temperatures).as_ptr();
+
+    let leaked_fahrenheit_temperatures = Vec::leak(fahrenheit_temperatures).as_ptr();
+
+    sp.fit(leaked_celsius_temperatures,
+        celsius_temperatures_row,
+        celsius_temperatures_col,
+        leaked_fahrenheit_temperatures,
+        fahrenheit_temperatures_row,
+        0.001,
+        100);
+
+        for i in 0..=100 {
+            let celsius = i as f32;
+            println!("{}C = prdicted_value={}, expected_value={}", celsius, sp.guess(vec![celsius]), celsius_to_fahrenheit(celsius));
+        }
+
+
 }
--- mylib/src/linear_model.rs
+++ mylib/src/linear_model.rs
@@ -10,9 +10,9 @@
 /*                                                                                                           */
 /* ********************************************************************************************************* */
 
-use std::fs::File;
-use std::io::prelude::*;
-use crate::utils::model::{Predict, Fit, GetWeights, SetWeights, ToString, Save, Load, GetHistory};
+// use std::ffi::{c_char, CStr};
+// use std::fs::File;
+// use std::io::prelude::*;
 
 pub struct LinearModel
 {
@@ -27,107 +27,80 @@ impl LinearModel {
             history: vec![],
         }
     }
-}
 
-impl Predict for LinearModel
-{
-    #[no_mangle]
-    extern "C" fn guess(&self, inputs: &[f32]) -> f32
+    fn get_weights(&self) -> Vec<f32>
+    {
+        self.weights.clone()
+    }
+    
+    fn set_weights(&mut self, weights: Vec<f32>)
+    {
+        self.weights = weights;
+    }
+
+    pub fn guess(&self, inputs: Vec<f32>) -> f32
     {
         let mut sum: f32 = 0.0;
         for (p,i) in self.weights.iter().skip(1).zip(inputs) {
-            sum = sum + *i * *p;
+            sum = sum + i * *p;
         }
         sum = sum + self.weights[0];
         sum
     }
-    #[no_mangle]
-    extern "C" fn predict(&self, inputs: &[f32]) {
 
-    }
-}
-
-impl Fit for LinearModel
-{
     #[no_mangle]
-    extern "C" fn fit(&mut self, datasets: (&Vec<Vec<f32>>, &Vec<f32>), learning_rate: f32, epochs: u32, _tolerance: f32) {
-        let (labels, features) = datasets;
-        self.weights = vec![0.0; labels[0].len() + 1];
+    pub extern "C" fn fit(&mut self, labels: *const f32, label_row: usize, label_col: usize, features: *const f32, feature_size: usize, learning_rate: f32, epochs: u32) {
+        let labels = unsafe {std::slice::from_raw_parts(labels, (label_col * label_row) as usize)};
+        let features = unsafe {std::slice::from_raw_parts(features, feature_size as usize)};
+    
+        self.weights = vec![0.0; label_row + 1];
         let mut epochs = epochs;
         let mut time = 0;
+        let data_size = if label_row < feature_size {label_row} else {feature_size};
+
         while epochs != 0 {
-            for (label, desired_output) in labels.iter().zip(features) {
-                let predicted_output = self.guess(&label);
-                let error = (desired_output - predicted_output).abs();
-                for i in 0..label.len() {
-                    self.weights[i+1] = self.weights[i] + learning_rate * (desired_output - predicted_output) * label[i];
+            let mut count_label = 0;
+            for count_feature in 0..data_size {
+                let desired_output = features[count_feature];
+
+                let mut input: Vec<f32> = vec![];
+                for j in 0..label_col {
+                    let elem = labels[count_label + j];
+                    input.push(elem);
                 }
-                self.weights[0] = self.weights[0] + learning_rate * (desired_output - predicted_output);
-                println!("{}", self.to_string());
+                count_label  = count_label + label_col;
+                let predicted_output = self.guess(input.clone());
+                let error = (desired_output - predicted_output).abs();
+                let mut weights: Vec<f32> = vec![0.0];
+                for j in 0..label_col {
+                    weights.push(self.weights[j+1] + learning_rate * (desired_output - predicted_output) * input[j]);
+                }
+                weights[0] = self.weights[0] + learning_rate * (desired_output - predicted_output);
+                self.set_weights(weights);
                 self.history.push(error);
                 time = time + 1;
             }
             epochs = epochs - 1;
         }
     }
-}
 
-impl GetWeights for LinearModel
-{
     #[no_mangle]
-    extern "C" fn get_weights(&self) -> Vec<f32>
-    {
-        self.weights.clone()
-    }
-}
-
-impl SetWeights for LinearModel
-{
-    #[no_mangle]
-    extern "C" fn set_weights(&mut self, weights: Vec<f32>)
-    {
-        self.weights = weights;
-    }
-}
-
-impl ToString for LinearModel
-{
-    #[no_mangle]
-    extern "C" fn to_string(&self) -> String
-    {
+    pub extern "C" fn to_string(&self) -> *const u8 {
+        let weights = self.get_weights();
         let mut res = "".to_string();
-        for w in &self.weights {
+        for w in weights {
             res.push_str(&w.to_string());
             res.push_str(",");
         }
-        res
+        let leaked_res = String::leak(res);
+        leaked_res.as_ptr()
     }
-}
 
-impl Save for LinearModel
-{
     #[no_mangle]
-    extern "C" fn save(&self, filepath: &str) -> std::io::Result<()>
+    pub extern "C" fn get_history(&self) -> *const f32
     {
-        let mut file = File::create(filepath)?;
-        file.write_all(self.to_string().as_bytes())?;
-        Ok(())
+        let leaked_arr = Vec::leak(self.history.clone());
+        leaked_arr.as_ptr()
     }
-}
-
-impl Load for LinearModel
-{
-    #[no_mangle]
-    extern "C" fn load(&mut self, filepath: &str)
-    {
-    }
-}
-
-impl GetHistory for LinearModel
-{
-    #[no_mangle]
-    extern "C" fn get_history(&self) -> Vec<f32>
-    {
-        self.history.clone()
-    }
+    
 }
