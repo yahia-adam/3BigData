72c6c073d8c87c158f81ad5e1df6a6a01a39ff0b
c-salhab <charbel.salhab.2003@gmail.com>
2024-05-27 14:30:18 +0200

    update gitignore to ignore ipynb folders

---
+++ mylib/src/support_vector_machine.rs
@@ -0,0 +1,7 @@
+/*
+ *  Author : Charbel SALHAB, Theo ELOY, Adam Yahia Abchafaa
+ *  Projet Annuel : Classification de type de déchets (plastique, papier, verre, métal,
+ *  déchet organique,déchet électrique, déchet dangereux (pile, produit chimique), autres).
+ *  3IABD1
+ *  2023-2024
+*/
