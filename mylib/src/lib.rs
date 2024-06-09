298bf6fab2dc20acef5a043d8b7eb355dced2e2e
c-salhab <charbel.salhab.2003@gmail.com>
2024-06-09 12:29:55 +0200

    update lib.rs file

--- mylib/src/lib.rs
+++ mylib/src/lib.rs
@@ -23,4 +23,7 @@ pub use multilayer_perceptron::{MultiLayerPerceptron, init_mlp, train_mlp, predi
 pub use linear_model::{LinearModel, init_linear_model, train_linear_model, predict_linear_model,free_linear_model, save_linear_model, load_linear_model};
 
 #[allow(unused_imports)]
-pub use radical_basis_function_network::{RadicalBasisFunctionNetwork, init_rbf, train_rbf_regression, train_rbf_rosenblatt, predict_rbf_regression, predict_rbf_classification};
+pub use radical_basis_function_network::{RadicalBasisFunctionNetwork, init_rbf,
+                                         train_rbf_regression, train_rbf_rosenblatt,
+                                         predict_rbf_regression, predict_rbf_classification,
+                                         free_rbf, save_rbf_model, rbf_to_json};
