dd05e06737607072bae1d3d5d8ef2f1e2a080137
Théo ELOY <theoeloy12@gmail.com>
2024-06-10 23:05:50 +0200

    Start to fix multi-classification

--- interoperability/python_interlop/wrapper.py
+++ interoperability/python_interlop/wrapper.py
@@ -88,6 +88,9 @@ class MyModel:
 
         data_size = len(y)
 
+        if self.__is_3_classes:
+            y[y == 0] = -1
+
         self.train_data = list(map(list, zip(*x)))
         self.train_data.append(y)
 
@@ -99,9 +102,10 @@ class MyModel:
             if self.__type == "ml":
                 my_lib.train_linear_model(self.model, x_flat_ptr, y_flat_ptr, data_size, learning_rate, epochs)
         else:
+            y = np.transpose(y)
+
             for i in range(3):
                 y_flat_ptr = y[i].flatten().astype(ctypes.c_float).ctypes.data_as(ctypes.POINTER(ctypes.c_float))
-
                 if self.__type == "ml":
                     my_lib.train_linear_model(self.model[i], x_flat_ptr, y_flat_ptr, self.__dims, learning_rate, epochs)
 
@@ -131,12 +135,14 @@ class MyModel:
                     if self.__type == "ml":
                         for i in range(3):
                             prediction.append(my_lib.predict_linear_model(self.model[i], points_pointer))
-                    if prediction[0] == 1:
+                    if prediction[0] == [1.0, -1.0, -1.0]:
                         background_colors.append("lightblue")
-                    elif prediction[1] == 1:
+                    elif prediction[1] == [-1.0, 1.0, -1.0]:
                         background_colors.append("pink")
-                    else:
+                    elif prediction[2] == [-1.0, -1.0, 1.0]:
                         background_colors.append("lightgreen")
+                    else:
+                        background_colors.append("white")
 
                 # if self.__type == "ml":
                 #     prediction = []
@@ -148,6 +154,7 @@ class MyModel:
 
                 pos_y += step
             pos_x += step
+
         background_points = np.array(background_points)
         plt.scatter(background_points[:, 0], background_points[:, 1], c=background_colors)
 
@@ -159,13 +166,10 @@ class MyModel:
                 else:
                     train_colors.append("red")
         else:
-            for result in self.train_data[2]:
-                result = np.array(result)
-                if np.all(result == result[0]):
-                    train_colors.append("white")
-                elif np.argmax(result) == 0:
+            for point in self.train_data[2]:
+                if point[0] == 1:
                     train_colors.append("blue")
-                elif np.argmax(result) == 1:
+                elif point[1] == 1:
                     train_colors.append("red")
                 else:
                     train_colors.append("green")
