7bb1c082cfa66746c1da10b84fd8ad6dafece194
c-salhab <charbel.salhab.2003@gmail.com>
2024-07-04 12:26:29 +0200

    rbf linear simple test case rust

--- mylib/examples/rbf/classification/linear_simple.rs
+++ mylib/examples/rbf/classification/linear_simple.rs
@@ -1,8 +1,6 @@
 #[allow(unused_imports)]
-use mylib::{RadicalBasisFunctionNetwork, init_rbf,
-            train_rbf_regression, train_rbf_rosenblatt,
-            predict_rbf_regression, predict_rbf_classification,
-            free_rbf, save_rbf_model, rbf_to_json };
+use mylib::{RadicalBasisFunctionNetwork, init_rbf, train_rbf_rosenblatt,
+            predict_rbf_classification, free_rbf, save_rbf_model, rbf_to_json};
 
 fn main() {
     let x: Vec<Vec<f32>> = vec![
@@ -15,22 +13,29 @@ fn main() {
         -1.0,
         -1.0
     ];
-    let data_size = y.len();
+    let sample_count = y.len() as i32;
+    let input_dim = x[0].len() as i32;
+    let cluster_num = 2;
+    let gamma = 1.0;
 
-    let x_flaten: Vec<f32> = x.clone().into_iter().flatten().collect::<Vec<f32>>();
-    let x_ptr: *const f32 = Vec::leak(x_flaten.clone()).as_ptr();
-    let y_ptr: *const f32 = Vec::leak(y.clone()).as_ptr();
-    
-    let rbf_model: *mut RadicalBasisFunctionNetwork = init_rbf(data_size, 2, 2);
-    train_rbf_regression(rbf_model, x_ptr, y_ptr, data_size as u32, 0.01, 1000);
-    
+    let mut x_flatten: Vec<f32> = x.clone().into_iter().flatten().collect();
+    let x_ptr: *mut f32 = x_flatten.as_mut_ptr();
+    let y_ptr: *mut f32 = y.clone().as_mut_ptr();
+
+    let rbf_class_model: *mut RadicalBasisFunctionNetwork = init_rbf(input_dim, cluster_num, gamma);
+    train_rbf_rosenblatt(rbf_class_model, x_ptr, y_ptr, 1000, 0.01, input_dim, sample_count);
+
+    println!("RBF Classification Model: OK");
     println!("");
-    println!("Linear Simple : Linear Model : OK");
-    println!("");
-    for i in 0..data_size {
-        let input_ptr: *mut f32 = Vec::leak(x[i].clone()).as_mut_ptr();
-        let output = predict_linear_model(linear_model, input_ptr);
-        println!("X:{:?}, Y:{:?} ---> mon model: {:?}", x[i], y[i], output);
+    for i in 0..sample_count as usize {
+        let input_ptr: *mut f32 = x[i].clone().as_mut_ptr();
+        let output = predict_rbf_classification(rbf_class_model, input_ptr);
+        println!("X: {:?}, Y: {:?} ---> RBF classification: {:?}", x[i], y[i], output);
     }
     println!("");
+
+    let loaded_model = rbf_to_json(b"rbf_model.json\0".as_ptr() as *const i8);
+
+    free_rbf(rbf_class_model);
+    free_rbf(loaded_model);
 }
