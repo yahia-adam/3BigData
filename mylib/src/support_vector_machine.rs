4bdc1d6926edd35a157f5b92b58bcf3e0f0ab503
Théo ELOY <theoeloy12@gmail.com>
2024-07-05 09:58:04 +0200

    merge

--- mylib/src/support_vector_machine.rs
+++ mylib/src/support_vector_machine.rs
@@ -10,36 +10,43 @@
 /*                                                                                                           */
 /* ********************************************************************************************************* */
 
-// use osqp::{CscMatrix, Problem, Settings};
-//
-// pub struct SVMModel{
-//
-// }
-// fn main() {
-//
-//     // Define problem data
-//     let p = &[[4.0, 1.0],
-//         [1.0, 2.0]];
-//     let q = &[1.0, 1.0];
-//     let a = &[[1.0, 1.0],
-//         [1.0, 0.0],
-//         [0.0, 1.0]];
-//     let l = &[1.0, 0.0, 0.0];
-//     let u = &[1.0, 0.7, 0.7];
-//
-//     // Extract the upper triangular elements of `P`
-//     let p = CscMatrix::from(p).into_upper_tri();
-//
-//     // Disable verbose output
-//     let settings = Settings::default()
-//         .verbose(false);
-//
-//     // Create an OSQP problem
-//     let mut prob = Problem::new(p, q, a, l, u, &settings).expect("failed to setup problem");
-//
-//     // Solve problem
-//     let result = prob.solve();
-//
-//     // Print the solution
-//     println!("{:?}", result.x().expect("failed to solve problem"));
-// }
+use osqp::{CscMatrix, Problem, Settings};
+
+pub struct SVMModel{
+    weight:f32,
+    biais:f32,
+}
+fn main() {
+
+    // Define problem data
+    let P = &[[4.0, 1.0],
+        [1.0, 2.0]];
+    let q = &[1.0, 1.0];
+    let A = &[[1.0, 1.0],
+        [1.0, 0.0],
+        [0.0, 1.0]];
+    let l = &[1.0, 0.0, 0.0];
+    let u = &[1.0, 0.7, 0.7];
+
+    // Extract the upper triangular elements of `P`
+    let P = CscMatrix::from(P);
+
+    // Disable verbose output
+    let settings = Settings::default()
+        .verbose(false);
+
+    // Create an OSQP problem
+    let mut prob = Problem::new(P, q, A, l, u, &settings).expect("failed to setup problem");
+
+    // Solve problem
+    let result = prob.solve();
+
+    // Print the solution
+    println!("{:?}", result.x().expect("failed to solve problem"));
+
+
+}
+
+fn train(model: SVMModel, input: &[f32], labels: &[f32], gamma: &f32, kernel: &str){
+
+}
