328c39932e40728aa594518955c0fe9efea073b0
Théo ELOY <theoeloy12@gmail.com>
2024-07-17 10:39:32 +0200

    move to new branch

--- mylib/src/support_vector_machine.rs
+++ mylib/src/support_vector_machine.rs
@@ -10,7 +10,6 @@
 /*                                                                                                           */
 /* ********************************************************************************************************* */
 
-use nalgebra::DMatrix;
 use osqp::{CscMatrix, Problem, Settings};
 
 pub struct SVMModel {
@@ -67,8 +66,8 @@ fn main() {
         vec![4.0, 1.0],
         vec![4.0, 4.0],
     ]);
-    let y = &[1, 1, -1, -1, -1, ];
-    train(&model, &x, y, &2.0)
+    let y = vec![1f32, 1f32, -1f32, -1f32, -1f32, ];
+    train(&model, &x, &y, &2.0, 5, 2);
 }
 
 fn get_kernel(model: &SVMModel, xi: &Vec<f32>, xj: &Vec<f32>) -> f32 {
@@ -83,36 +82,71 @@ fn get_kernel(model: &SVMModel, xi: &Vec<f32>, xj: &Vec<f32>) -> f32 {
     */else { 0.0 }
 }
 
-fn train(model: &SVMModel, inputs: &Vec<Vec<f32>>, labels: &[i32], gamma: &f32) {
-    let mut big_matrix: Vec<Vec<f64>> = Vec::new();
 
-    for i in 0..inputs.len() {
-        big_matrix.push(Vec::new());
-        for j in 0..inputs.len() {
-            big_matrix[i].push((labels[i] as f32 * labels[j] as f32 * get_kernel(model, &inputs[i], &inputs[j])) as f64)
-        }
+fn train(model: &SVMModel, inputs: &Vec<Vec<f32>>, labels: &Vec<f32>, gamma: &f32, input_length: u32, dimensions: u32) {
+    let total_input:usize = (input_length * dimensions) as usize;
+    let dimensions = dimensions as usize;
+    let input_length = input_length as usize;
+
+
+    let mut big_matrix: Vec<Vec<f64>> = Vec::with_capacity(input_length);
+    for i in 0..input_length {
+        let row: Vec<f64> = (0..input_length)
+            .map(|j| {
+                let value = labels[i] as f32 * labels[j] as f32 * get_kernel(model, &inputs[i], &inputs[j]);
+                value as f64
+            })
+            .collect();
+        big_matrix.push(row);
     }
 
-    println!("Big Matrix: {:?}", big_matrix);
+    println!("P: {:?}", big_matrix);
 
-    let p = CscMatrix::from(&big_matrix);
+     let big_csc_matrix = CscMatrix::from(&big_matrix).into_upper_tri();
+    println!("P csc: {:?}", big_csc_matrix);
 
-    let q = vec![0f32; model.sample_size as usize].append(&mut vec![*gamma; inputs.len()]);
+    let mut q: Vec<f64> = vec![-1f64; input_length];
+    // let mut q: Vec<f64> = Vec::with_capacity(input_length);
+    // //q.extend(vec![0f64; input_length]);
+    // q.extend(vec![*gamma as f64; input_length]);
+
     println!("q :{:?}", q);
 
-    let identity = DMatrix::identity(model.sample_len as usize, model.sample_len as usize);
+    // let identity:OMatrix<f64, Dyn, Dyn> = DMatrix::identity(dimensions, dimensions);
 
-    const LENGTH: u32 = model.sample_len;
+    // let mut identity:Vec<Vec<f64>> = Vec::with_capacity(dimensions);
+    // for i in 0..dimensions{
+    //     let row:Vec<f64> = (0..dimensions).map(|j| {(i == j).into()}).collect();
+    //     identity.push(row);
+    // }
+    // println!("id: {:?}", identity);
 
-    let mut a_matrix = [[0f64; LENGTH * 2 + 1]; LENGTH];
 
-    for i in 0..LENGTH {
+    let mut a_matrix: Vec<Vec<f64>> = vec![vec![0f64; input_length]; (input_length * 2 + 1)];
+
+    for i in 0..input_length {
         a_matrix[0][i] = labels[i] as f64;
         a_matrix[i + 1][i] = 1f64;
-        a_matrix[i + LENGTH + 1][i] = -1f64;
+        a_matrix[i + input_length + 1][i] = -1f64;
     }
     println!("a :{:?}", a_matrix);
 
-    let settings = osqp::Settings::default().verbose(true);
-    //osqp::Problem::new
+    let mut l: Vec<f64> = Vec::with_capacity((input_length * 2 + 1) as usize);
+    l.extend(vec![-f64::INFINITY; input_length+1 ]);
+    l.extend(vec![0f64; input_length]);
+    println!("l: {:?}", l);
+
+    let mut u: Vec<f64> = Vec::with_capacity((input_length * 2 + 1) as usize);
+    u.extend(vec![1f64; input_length + 1]);
+    u.extend(vec![f64::INFINITY; input_length as usize]);
+
+
+    let settings = Settings::default().verbose(true);
+    let mut problem = Problem::new(&big_csc_matrix, &*q, &a_matrix, &*l, &*u, &settings).expect("OSQP Setup Error");
+
+    // Solve problem
+    let result = problem.solve();
+
+    // Print the solution
+    println!("{:?}", result.x().expect("failed to solve problem"));
 }
